4982651a46679dce864ec363e5b1a5d45af35ec1
François Degrave <francois.degrave@fundp.ac.be>
2017-04-11 16:07:20 +0200

    [IMP] description line

--- account_report_template/__manifest__.py
+++ account_report_template/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "University Modules Accounting Reports",
-    "version": "1.0",
+    "version": "0.1a",
     "depends": ["report_table", "account"],
     "author": "University of Namur",
     "category": "Accounting",
@@ -8,6 +8,8 @@
     This is a module doing the same things as the official Odoo module for accounting reports, but the inner working is
     completely different (the authors have never seen the code of the closed-source module).
     It depends on the generic table_report module (https://github.com/unamur-dev/odoo-tools).
+
+    THIS IS AN ALPHA VERSION. All feedback is welcome. It should be all finished and tested by September.
     """,
     'demo': [],
     'data': [
